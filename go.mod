51820f0167d9451d0904bf8c2d681d0e48a3634f
rs98065 <rahul.sharma2@kroger.com>
2021-12-24 12:14:41 -0800

    Added sample code with endpoints using httprouter library

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rssharma75/go_restful_apis
 
 go 1.16
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
--- main.go
+++ main.go
@@ -1,64 +1,19 @@
 package main
 
 import (
-	"encoding/base64"
-	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
-	"strings"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-type server struct {
-	user User
+func hello(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Hello World")
 }
 
-type User struct {
-	UserName string `json:username`
-	Email    string `json:email`
-	Age      int    `json:age`
-}
-
-type dog int
-
-func getBase64(w http.ResponseWriter, r *http.Request) {
-	msg := strings.Split(r.URL.String(), "/")[2]
-	data := []byte(msg)
-	str := base64.StdEncoding.EncodeToString(data)
-	w.Write([]byte(str))
-}
-func (s *server) getUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-type", "application/json")
-
-	switch r.Method {
-	case "GET":
-		e := json.NewEncoder(w)
-		e.Encode(s.user)
-		w.WriteHeader(http.StatusOK)
-	case "PUT":
-		var user User
-		err := json.NewDecoder(r.Body).Decode(&user)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		s.user = user
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"update":"OK"}`))
-
-	default:
-		w.WriteHeader(http.StatusNotImplemented)
-	}
-
-}
 func main() {
-	s := &server{
-		user: User{
-			UserName: "go_learner",
-			Email:    "go.learner@gmail.com",
-			Age:      10,
-		},
-	}
-	http.HandleFunc("/user", s.getUser)
-	http.HandleFunc("/base64/", getBase64)
-
-	http.ListenAndServe(":5000", nil)
+	router := httprouter.New()
+	router.GET("/hello", hello)
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
