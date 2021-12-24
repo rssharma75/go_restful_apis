b78d46ebc6d80f4fb608a8903bde3c8af31e9a99
rs98065 <rahul.sharma2@kroger.com>
2021-12-23 16:10:40 -0800

    Added Dog endpoint

--- main.go
+++ main.go
@@ -4,14 +4,22 @@ import "net/http"
 
 type server struct{}
 
+type dog int
+
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, World!"))
 }
 
+func (d dog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Dog"))
+}
+
 func main() {
 	s := &server{}
+	d := dog(10)
 
 	http.Handle("/", s)
+	http.Handle("/dog/", d)
 
 	http.ListenAndServe(":7999", nil)
 }
