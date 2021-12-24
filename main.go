9de5beb60a98735701ebbabb164c98706c363492
rs98065 <rahul.sharma2@kroger.com>
2021-12-24 12:05:31 -0800

    Added net/http code for simple endpoints

--- main.go
+++ main.go
@@ -1,35 +1,64 @@
 package main
 
-import "net/http"
+import (
+	"encoding/base64"
+	"encoding/json"
+	"net/http"
+	"strings"
+)
 
-type server struct{}
+type server struct {
+	user User
+}
+
+type User struct {
+	UserName string `json:username`
+	Email    string `json:email`
+	Age      int    `json:age`
+}
 
 type dog int
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
+func getBase64(w http.ResponseWriter, r *http.Request) {
+	msg := strings.Split(r.URL.String(), "/")[2]
+	data := []byte(msg)
+	str := base64.StdEncoding.EncodeToString(data)
+	w.Write([]byte(str))
 }
+func (s *server) getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-type", "application/json")
 
-func (d dog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Dog"))
+	switch r.Method {
+	case "GET":
+		e := json.NewEncoder(w)
+		e.Encode(s.user)
+		w.WriteHeader(http.StatusOK)
+	case "PUT":
+		var user User
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		s.user = user
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"update":"OK"}`))
+
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
+	}
+
 }
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Home"))
-}
-
-func (s *server) home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome Home"))
-}
-
 func main() {
-	s := &server{}
-	d := dog(10)
+	s := &server{
+		user: User{
+			UserName: "go_learner",
+			Email:    "go.learner@gmail.com",
+			Age:      10,
+		},
+	}
+	http.HandleFunc("/user", s.getUser)
+	http.HandleFunc("/base64/", getBase64)
 
-	http.Handle("/", s)
-	http.Handle("/dog/", d)
-
-	http.HandleFunc("/home", s.home)
-
-	http.ListenAndServe(":7999", nil)
+	http.ListenAndServe(":5000", nil)
 }
