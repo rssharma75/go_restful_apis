f6875067015789649651dc9a20d11aa63e35f9ea
rs98065 <rahul.sharma2@kroger.com>
2021-12-23 16:14:37 -0800

    Added handlerfunc endpoint

--- main.go
+++ main.go
@@ -14,12 +14,18 @@ func (d dog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Dog"))
 }
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Home"))
+}
+
 func main() {
 	s := &server{}
 	d := dog(10)
 
 	http.Handle("/", s)
 	http.Handle("/dog/", d)
+
+	http.HandleFunc("/home", home)
 
 	http.ListenAndServe(":7999", nil)
 }
